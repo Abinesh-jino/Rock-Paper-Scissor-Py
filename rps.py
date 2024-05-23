19cf6c7eea51f89c2a5bd71139a38fe1800aa0cf
Abinesh-jino <abineshjino1003@gmail.com>
2024-05-23 23:05:38 +0530

    Added More Personalised Args

--- rps.py
+++ rps.py
@@ -1,60 +1,82 @@
 import random
 import sys
 from enum import Enum
+import argparse
 
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSOR = 3
+parser=argparse.ArgumentParser(
+    description="Says Hello to User"
+)
+
+parser.add_argument(
+    "-n", "--name" ,"--jin" , metavar="name" ,
+    required=True , help="The Name of user Playing the Game "
+)
+
+args= parser.parse_args()
+
+realname=args.name
 
 
-print(" \n Welcome To Rock Paper Scissor Game 🎮 \n" .center(60,"-") )
+def play_rps(name='Player'):
+    
+    
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSOR = 3
 
-#Options for RPS 
 
-playagain = True
+    print(f"\n{name}, Welcome To Rock Paper Scissor Game 🎮 \n" .center(60,"-") )
 
-while playagain:
+    #Options for RPS 
 
-    playerchoice = input("\n Select from Below \n\t 1 for Rock 🪨 \n\t 2 for Paper 📃, or \n\t 3 for Scissors ✂️ \n \n Enter Your Choice : ")
+    playagain = True
 
-    #Casting into Int Values
-
-    player = int(playerchoice)
-
-    computerchoice = random.choice("123")
-    computer = int(computerchoice)
-
-    print(" ")
-
-    print(" You Chose " + str(RPS(player)).replace('RPS.', ' ').title() )
-    print(" Python Chose " + str(RPS(computer)).replace('RPS.', ' ').title() )
-
-    print("")
-
-    if player<1 or player > 3 :
-        sys.exit(" You Have to Only Enter 1 , 2 or 3 \n\n" )
-    elif player ==1 and computer==3 :
-        print(" Rock Beats Scissor \n")
-        print(" You win 🏆 🥳 \n\n  ")
-    elif player==2 and computer==1 :
-        print(" Paper Wraps Rock \n")
-        print(" You win 🏆 🥳\n\n  ")
-    elif player==3 and computer==2 :
-        print(" Scissor Cuts Paper \n")
-        print(" You win 🏆 🥳 \n\n  ")
-
-    elif player==computer:
-        print(" You Chose Same as Python  \n")
-        print( " 😲 It is a Tie , Play Again !! \n\n ")
-    else:
-        print(" 🐍 Python Wins , Try Again 🥹 \n ")
+    while playagain:
         
-    playagain = input(" \n Do You Want to Play Again ? (Y/N) : ").lower()
-    if playagain=='y':
-        playagain = True
-    else:
-        playagain = False
-        print(" \n Thank You For Playing Rock Paper Scissor Game 🎮 \n".center(60,"-") )
-        sys.exit(" \n\n Game Exited Successfully !! \n\n")
+
+        playerchoice = input(f"\n {name} , please select from Below \n\t 1 for Rock 🪨 \n\t 2 for Paper 📃, or \n\t 3 for Scissors ✂️ \n \n Enter Your Choice : ")
+
+        if
+        #Casting into Int Values
+
+        player = int(playerchoice)
+
+        computerchoice = random.choice("123")
+        computer = int(computerchoice)
+
+        print(" ")
+
+        print(f" {name} , you Chose {str(RPS(player)).replace('RPS.', ' ').title()} ")
+        print(f" Python Chose {str(RPS(computer)).replace('RPS.', ' ').title()}" )
+
+        print("")
+
+        if player<1 or player > 3 :
+            sys.exit(" {name} , please Only Enter 1 , 2 or 3 \n\n" )
+        elif player ==1 and computer==3 :
+            print(" Rock Beats Scissor \n")
+            print(f" {name} , you won 🏆 🥳 \n\n  ")
+        elif player==2 and computer==1 :
+            print(" Paper Wraps Rock \n")
+            print(f" {name} , you won 🏆 🥳 \n\n ")
+        elif player==3 and computer==2 :
+            print(" Scissor Cuts Paper \n")
+            print(f" {name} , you won  \n\n  ")
+
+        elif player==computer:
+            print(f" Ouch  {name} , you Chose Same as Python  \n")
+            print( " 😲 It is a Tie , Play Again !! \n\n ")
+        else:
+            print(f" Sorry {name} 🐍 Python Won , Try Again 🥹 \n ")
+            
+        playagain = input(f" \n {name} , do You Wanna Play Again ? (Y/N) : ").lower()
+        if playagain=='y':
+            playagain = True
+        else:
+            playagain = False
+            print(" \n Thank You For Playing Rock Paper Scissor Game 🎮 \n".center(60,"-") )
+            sys.exit(" \n\n Game Exited Successfully !! \n\n")
+
+play_rps(realname)
